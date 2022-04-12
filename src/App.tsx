3f5aef0da1ee45159ec2d2bd50301051708ec3c2
LautaAndreani <lautaroandreani2016@gmail.com>
2022-04-11 21:17:31 -0300

    fix: npm dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react';
-import List from './components/List';
-import Form from './components/Form';
-import { Sub } from './types/types.d';
-import { INITIAL_STATE } from './data';
+import { useEffect, useState } from "react"
+import List from "./components/List"
+import Form from "./components/Form"
+import { Sub } from "./types/types.d"
+import { INITIAL_STATE } from "./data"
 
-import './App.css';
+import "./App.css"
 
 //Interface
 
 interface AppState {
-  subs: Sub[];
+  subs: Sub[]
 }
 
 function App() {
-  const [subs, setSubs] = useState<AppState['subs']>([]);
-  console.log(subs);
+  const [subs, setSubs] = useState<AppState["subs"]>([])
+  console.log(subs)
 
   useEffect(() => {
-    setSubs(INITIAL_STATE);
-  });
+    setSubs(INITIAL_STATE)
+  })
 
   return (
     <div className="App">
       <h1>Sub descriptions</h1>
       <List subs={subs} />
-      <Form setSubs={setSubs} />
+      <Form onNewSub={setSubs} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,71 +1,45 @@
-import React, { useState } from 'react';
-import { Sub } from '../types/types';
+import React, { useState } from "react"
+import { Sub } from "../types/types"
 
 interface FormState {
-  inputValues: Sub;
+  inputValues: Sub
 }
 
 interface FormProps {
-  setSubs: React.Dispatch<React.SetStateAction<Sub[]>>;
+  onNewSub: React.Dispatch<React.SetStateAction<Sub[]>>
 }
 
-const Form = ({ setSubs }: FormProps) => {
-  const [inputValues, setInputValues] = useState<FormState['inputValues']>({
-    nick: '',
+const Form = ({ onNewSub }: FormProps) => {
+  const [inputValues, setInputValues] = useState<FormState["inputValues"]>({
+    nick: "",
     subMonths: 0,
-    avatar: '',
-    description: '',
-  });
+    avatar: "",
+    description: "",
+  })
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setSubs((subs) => [...subs, inputValues]);
-  };
+    e.preventDefault()
+    onNewSub((subs) => [...subs, inputValues])
+  }
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setInputValues({
       ...inputValues,
       [e.target.name]: e.target.value,
-    });
-  };
+    })
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleChange}
-          value={inputValues.nick}
-          type="text"
-          name="nick"
-          placeholder="nick"
-        />
-        <input
-          onChange={handleChange}
-          value={inputValues.subMonths}
-          type="number"
-          name="subMonths"
-          placeholder="subMonths"
-        />
-        <input
-          onChange={handleChange}
-          value={inputValues.avatar}
-          type="text"
-          name="avatar"
-          placeholder="avatar"
-        />
-        <textarea
-          onChange={handleChange}
-          value={inputValues.description}
-          name="description"
-          placeholder="description"
-        />
-
-        <button type="submit">Save new sub !</button>
+        <input onChange={handleChange} value={inputValues.nick} type="text" name="nick" placeholder="nick" />
+        <input onChange={handleChange} value={inputValues.subMonths} type="number" name="subMonths" placeholder="subMonths" />
+        <input onChange={handleChange} value={inputValues.avatar} type="text" name="avatar" placeholder="avatar" />
+        <textarea onChange={handleChange} value={inputValues.description} name="description" placeholder="description" />
+        <button>Save new sub !</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Form;
+export default Form
